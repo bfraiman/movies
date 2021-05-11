025f3f1640029ac82ee773102e225d7c53a3290c
Brian <bfraiman@Brians-MBP.fibertel.com.ar>
2021-05-10 23:54:55 -0300

    Create Movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,21 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Movie = ({ data }) => {
-  const src = `https://image.tmdb.org/t/p/original/${data.poster_path}`
+  const src = `https://image.tmdb.org/t/p/original/${data.poster_path}`;
   return (
     <div className="movie">
-    <img
-      width="200"
-      alt={data.title}
-      src={src}
-    />
+      <img width="100px" height="140px" alt={data.title} src={src} />
+      <div className="movie-overview">
+        <p className="movie-overview-title">{data.title}</p>
+      </div>
     </div>
   );
 };
 
 Movie.propTypes = {
   data: PropTypes.shape({
-    // title: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
     // overview: PropTypes.string.isRequired,
     // vote_average: PropTypes.number.isRequired,
     poster_path: PropTypes.string.isRequired,
